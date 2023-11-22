ae348ea12e9d772dac2f8faa4b4a6a230e5c1a28
feardamhan <35301475+feardamhan@users.noreply.github.com>
2023-11-22 23:03:16 +0000

    Update VCFInstanceRecovery.psm1

--- VCFInstanceRecovery.psm1
+++ VCFInstanceRecovery.psm1
@@ -1521,10 +1521,10 @@ Function Invoke-SDDCManagerRestore
         $Counter = 0
         Do
         {
-            $SddcManagerServiceStatus = (Invoke-SSHCommand -timeout 30 -sessionid $sshSession.SessionId -command $scriptText).output
+            $Global:SddcManagerServiceStatus = (Invoke-SSHCommand -timeout 30 -sessionid $sshSession.SessionId -command $scriptText).output
             $operationsManagerServiceStatus = (($SddcManagerServiceStatus | ConvertFrom-Json).elements | Where-Object {$_.name -eq "OPERATIONS_MANAGER"}).status
-            If ($operationsManagerServiceStatus -ne "UP") {$counter ++; Write-Host "operationsManagerServiceStatus status is $operationsManagerServiceStatus. Wait $counter"; Sleep 10;}
-        } Until ($operationsManagerServiceStatus -eq "UP")
+            If ($operationsManagerServiceStatus -ne "UP") {$counter ++; Write-Host "operationsManagerServiceStatus status is $operationsManagerServiceStatus. Wait $counter"; Sleep 10}
+        } While ($operationsManagerServiceStatus -ne "UP")
         $scriptText = "curl https://$extractedSddcManagerFqdn/v1/restores/tasks -k -X POST -H `"Content-Type: application/json`" -H `"Authorization: Bearer $token`" -d `'{`"elements`" : [ {`"resourceType`" : `"SDDC_MANAGER`"} ],`"backupFile`" : `"/tmp/$backupFileName`",`"encryption`" : {`"passphrase`" : `"$localUserPassword`"}}`' | json_pp | jq `'.id`' | cut -d `'`"`' -f 2"
         Do
         {
