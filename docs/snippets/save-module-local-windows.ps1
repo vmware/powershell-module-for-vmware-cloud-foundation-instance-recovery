5f6169eae5964a7682ec7840c8014c831cc254ac
BrianOConnell <brianoc4@gmail.com>
2024-09-02 15:56:46 +0100

    chore-release-1.0.12
    Updates for release
    Signed-off-by: Brian O'Connell <brianoc4@gmail.com>

--- docs/snippets/save-module-local-windows.ps1
+++ docs/snippets/save-module-local-windows.ps1
@@ -1,4 +1,4 @@
-Save-Module -Name VMware.PowerCLI -RequiredVersion 13.1.0 -Path F:\Module\ -Repository PSGallery
+Save-Module -Name VMware.PowerCLI -RequiredVersion 13.3.0 -Path F:\Module\ -Repository PSGallery
 Save-Module -Name 7Zip4PowerShell -RequiredVersion 2.4.0 -Path F:\Module\ -Repository PSGallery
 Save-Module -Name Posh-SSH -RequiredVersion 3.0.8 -Path F:\Module\ -Repository PSGallery
 Save-Module -Name VMware.CloudFoundation.InstanceRecovery -Path F:\Module\ -Repository PSGallery
