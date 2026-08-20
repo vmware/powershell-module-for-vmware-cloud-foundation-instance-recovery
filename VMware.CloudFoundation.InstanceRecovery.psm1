3e3fd7842210313bf1d249693a45b5491d1cf98c
Ken Gould <gould.ken@gmail.com>
2026-08-20 17:47:55 +0100

    Revert "force host mapping add"
    This reverts commit 012a6cf3bc1d38ad47acc6489a99ec973689241f.
    Signed-off-by: Ken Gould <gould.ken@gmail.com>

--- VMware.CloudFoundation.InstanceRecovery.psm1
+++ VMware.CloudFoundation.InstanceRecovery.psm1
@@ -1395,7 +1395,7 @@ Function Update-ExtractedSDDCData {
             $azHostMappingObject = New-Object -type psobject
             $azHostMappingObject | Add-Member -NotePropertyName "az1" -NotePropertyValue $az1Hosts
             $azHostMappingObject | Add-Member -NotePropertyName "az2" -NotePropertyValue $az2Hosts
-            $cluster | Add-Member -NotePropertyName "azHostMapping" -NotePropertyValue $azHostMappingObject -force
+            $cluster | Add-Member -NotePropertyName "azHostMapping" -NotePropertyValue $azHostMappingObject
         }
         Disconnect-VIServer * -confirm:$false
     }
