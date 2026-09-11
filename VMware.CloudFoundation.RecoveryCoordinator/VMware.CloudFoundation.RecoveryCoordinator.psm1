a3c354f0bb6263caac4b336c6c6c1a74bc736fa2
Ken Gould <ken.gould@broadcom.com>
2026-09-11 17:11:58 +0100

    Revert "Remove function exports"
    This reverts commit 5564b70a3027bd0ea68d1a81c9de8c2ac10ec737.

--- VMware.CloudFoundation.RecoveryCoordinator/VMware.CloudFoundation.RecoveryCoordinator.psm1
+++ VMware.CloudFoundation.RecoveryCoordinator/VMware.CloudFoundation.RecoveryCoordinator.psm1
@@ -117,6 +117,7 @@ Function Import-RecoveryVariables {
     $minutes = (($StopWatch.Elapsed.Hours * 60) + $StopWatch.Elapsed.Minutes)
     LogMessage -type NOTE -message "[$jumpboxName] Completed Task $($MyInvocation.MyCommand) in $minutes minutes and $($StopWatch.Elapsed.Seconds) seconds"
 }
+Export-ModuleMember -Function Import-RecoveryVariables
 
 Function Set-ExportedSDDCDataFilePath {
     <#
@@ -149,6 +150,7 @@ Function Set-ExportedSDDCDataFilePath {
     $minutes = (($StopWatch.Elapsed.Hours * 60) + $StopWatch.Elapsed.Minutes)
     LogMessage -type NOTE -message "[$jumpboxName] Completed Task $($MyInvocation.MyCommand) in $minutes minutes and $($StopWatch.Elapsed.Seconds) seconds"
 }
+Export-ModuleMember -Function Set-ExportedSDDCDataFilePath
 
 Function Set-SelectedRecoveryTarget {
     <#
@@ -205,6 +207,7 @@ Function Set-SelectedRecoveryTarget {
     $minutes = (($StopWatch.Elapsed.Hours * 60) + $StopWatch.Elapsed.Minutes)
     LogMessage -type NOTE -message "[$jumpboxName] Completed Task $($MyInvocation.MyCommand) in $minutes minutes and $($StopWatch.Elapsed.Seconds) seconds"
 }
+Export-ModuleMember -Function Set-SelectedRecoveryTarget
 
 #EndRegion Recovery Variables
 
