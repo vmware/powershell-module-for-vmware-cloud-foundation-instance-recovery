dc44eec59e5b720a4abf6154698c5451da4d2636
Ken Gould <ken.gould@broadcom.com>
2026-09-21 16:53:08 +0100

    fix typos
    .
    Signed-off-by: Ken Gould <ken.gould@broadcom.com>

--- VMware.CloudFoundation.InstanceRecovery.psm1
+++ VMware.CloudFoundation.InstanceRecovery.psm1
@@ -10745,11 +10745,11 @@ Function Get-VcfOperationsRegisteredComponents {
     LogMessage -type INFO -message "[$VcfOperationsFqdn] Found $($filtered.Count) filtered component(s) of types: $($targetTypes -join ', '); $($vspComponents.Count) VSP instance(s)"
 
     $json = $result | ConvertTo-Json -Depth 10 | Out-File $outputFile
-    LogMessage -type INFO -message "[$VcfOperationsFqdn] VCF Management Services Runtime VSP ID: $($vsp.vspComponentUuid)"
+    LogMessage -type INFO -message "[$VcfOperationsFqdn] VCF Management Services Runtime VSP ID: $($vsp.id)"
     LogMessage -type INFO -message "[$VcfOperationsFqdn] VCF Management Services Runtime Component Version: $($vsp.componentVersion)"
     If ($vcfa)
     {
-        LogMessage -type INFO -message "[$VcfOperationsFqdn] VCF Automation Services Runtime VSP ID: $($vcfa.componentUuid)"
+        LogMessage -type INFO -message "[$VcfOperationsFqdn] VCF Automation Services Runtime VSP ID: $($vcfa.vspComponentUuid)"
         LogMessage -type INFO -message "[$VcfOperationsFqdn] VCF Automation Services Runtime Component Version: $($vcfa.componentVersion)"
     }
     $StopWatch.Stop()
